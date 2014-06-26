f154e382f9f2c674c9c3a8e9a3f5e2d0c3ca16a0
juan2ramos <juan2ramos@gmail.com>
2014-06-26 18:04:50 -0500

    mail

--- email.php
+++ email.php
@@ -1,5 +1,13 @@
 <?php
+// El mensaje
+$mensaje = "Línea 1\r\nLínea 2\r\nLínea 3";
 
+// Si cualquier línea es más larga de 70 caracteres, se debería usar wordwrap()
+$mensaje = wordwrap($mensaje, 70, "\r\n");
+
+// Enviarlo
+mail('info@mi-martinez.com', 'Mi título', $mensaje);
+/*
 if (!empty($_POST)) {
 
     if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['comment']) ){
